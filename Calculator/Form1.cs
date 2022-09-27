bc06ef502f1ffb26eb56d233e0dd154b2e6ab6d9
Fulin <944799613@qq.com>
2022-09-27 15:27:56 -0500

    if

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -3,104 +3,115 @@ namespace Calculator
     public partial class Form1 : Form
     {
         //表达式变量
-        public static string ShowResult = "";
+        public static string ShowTextBox = "";
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void NumAndSymbol(string num)
+        {
+            //判断输入的是不是数字或者小数点
+            string numAndPoint = "0,1,2,3,4,5,6,7,8,9,.";
+            //返回当前字符在字符串中的位置 
+            if(numAndPoint.IndexOf(num) >= 0)
+            {
+                // 说明 是 "0,1,2,3,4,5,6,7,8,9,." 这一堆
+
+            }
+            else
+            {
+                // 说明 不是"0,1,2,3,4,5,6,7,8,9,." 这一堆，而是符号
+
+            }
+
+
+            //全局表达式变量累计
+            ShowTextBox += num;
+            result.Text = ShowTextBox;
+        }
+
         private void result_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
         private void n7_Click(object sender, EventArgs e)
         {
-            ShowResult += "7";
-            result.Text = ShowResult;
+            NumAndSymbol("7");
         }
 
         private void n8_Click(object sender, EventArgs e)
         {
-            ShowResult += "8";
-            result.Text = ShowResult;
+            NumAndSymbol("8");
         }
 
         private void n9_Click(object sender, EventArgs e)
         {
-            ShowResult += "9";
-            result.Text = ShowResult;
+            NumAndSymbol("9");
         }
 
         private void n4_Click(object sender, EventArgs e)
         {
-            ShowResult += "4";
-            result.Text = ShowResult;
+            NumAndSymbol("4");
         }
 
         private void n5_Click(object sender, EventArgs e)
         {
-            ShowResult += "5";
-            result.Text = ShowResult;
+            NumAndSymbol("5");
         }
 
         private void n6_Click(object sender, EventArgs e)
         {
-            ShowResult += "6";
-            result.Text = ShowResult;
+            NumAndSymbol("6");
         }
         private void n1_Click(object sender, EventArgs e)
         {
-            ShowResult += "1";
-            result.Text = ShowResult;
+            NumAndSymbol("1");
         }
 
         private void n2_Click(object sender, EventArgs e)
         {
-            ShowResult += "2";
-            result.Text = ShowResult;
+            NumAndSymbol("2");
         }
 
         private void n3_Click(object sender, EventArgs e)
         {
-            ShowResult += "3";
-            result.Text = ShowResult;
+            NumAndSymbol("3");
         }
 
         private void n0_Click(object sender, EventArgs e)
         {
-            ShowResult += "0";
-            result.Text = ShowResult;
-        }
-
-        private void clear_Click(object sender, EventArgs e)
-        {
-            
+            NumAndSymbol("0");
         }
 
         private void point_Click(object sender, EventArgs e)
         {
-            // 只能加一次
-            result.Text = result.Text + ".";
+            NumAndSymbol(".");
         }
         private void add_Click(object sender, EventArgs e)
         {
-
+            NumAndSymbol("+");
         }
         private void minus_Click(object sender, EventArgs e)
         {
-
+            NumAndSymbol("-");
         }
 
         private void multiply_Click(object sender, EventArgs e)
         {
-
+            NumAndSymbol("*");
         }
 
         private void divide_Click(object sender, EventArgs e)
         {
-
+            NumAndSymbol("/");
         }
 
         private void equal_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void clear_Click(object sender, EventArgs e)
         {
 
         }
